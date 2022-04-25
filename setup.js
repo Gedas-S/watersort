c707d949ec8decb9eb6c811bd4e89f5f1339599c
Gedas-S <Gedas-S@users.noreply.github.com>
2022-04-25 21:05:33 +0300

    fix level gen

--- setup.js
+++ setup.js
@@ -93,7 +93,7 @@ function make_level(n) {
             }
             amount = free_space[targets[target_index]]
         }
-        if (!split && source.children.length == 1) {
+        if (!split && source.children.length > 1) {
             // the move is non-reversible, abort
             continue
         }
